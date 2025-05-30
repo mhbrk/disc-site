d8553d08a6a3b2ce8404f02a150a4ea867dd42d9
Yason Khaburzaniya <yason@hey.com>
2025-05-29 19:02:46 -0700

    move generator agent into the app

--- breba_app/app/orchestrator.py
+++ breba_app/app/orchestrator.py
@@ -1,14 +1,18 @@
+import logging
 import os
 
 from builder_agent.agent import BuilderAgent
-
-from common.constants import ASK_CHAT_AGENT_TOPIC, BUILDER_AGENT_TOPIC
-from common.model import TextPart, Message, Artifact, TaskStatus, TaskState, Task, SendTaskResponse, TaskSendParams, SendTaskStreamingRequest
+from common.constants import ASK_CHAT_AGENT_TOPIC, BUILDER_AGENT_TOPIC, GENERATOR_AGENT_TOPIC
+from common.model import TextPart, Message, Artifact, TaskStatus, TaskState, Task, SendTaskResponse, TaskSendParams, \
+    SendTaskStreamingRequest, TaskArtifactUpdateEvent, SendTaskStreamingResponse
 from common.utils import publish_to_topic
+from generator_agent.accumulator import TagAccumulator
+from generator_agent.agent import HTMLAgent
 
-import httpx
+logger = logging.getLogger(__name__)
 
 builder_agent = BuilderAgent()
+generator_agent = HTMLAgent()
 
 
 async def publish_task_request(task_id: str, session_id: str, message: str):
@@ -61,6 +65,54 @@ async def publish_task_response(task_id: str, session_id: str, content: str, tas
     await publish_to_topic(ASK_CHAT_AGENT_TOPIC, response.model_dump(exclude_none=True), task_id)
 
 
+async def publish_artifact_update(task_id: str, session_id: str, content: str):
+    artifact = Artifact(parts=[TextPart(text=content)])
+
+    update = TaskArtifactUpdateEvent(id=task_id, artifact=artifact, metadata={"sessionId": session_id})
+
+    response = SendTaskStreamingResponse(result=update)
+
+    await publish_to_topic(GENERATOR_AGENT_TOPIC, response.model_dump(exclude_none=True), task_id)
+
+
+async def publish_generator_completed(task_id: str, session_id: str, content: str):
+    task_status = TaskStatus(state=TaskState.COMPLETED)
+    artifact = Artifact(parts=[TextPart(text=content)])
+
+    task = Task(
+        id=task_id,
+        sessionId=session_id,
+        status=task_status,
+        artifacts=[artifact],
+        metadata={}
+    )
+
+    response = SendTaskResponse(result=task)
+    await publish_to_topic(GENERATOR_AGENT_TOPIC, response.model_dump(exclude_none=True), task_id)
+
+
+async def start_streaming_task(task_id: str, session_id: str, query: str):
+    accumulator = TagAccumulator()
+    async for chunk in generator_agent.stream(query, session_id, task_id):
+        logger.info(f"Processing chunk from agent: {chunk}")
+        content = chunk.get("content")
+        if not content:
+            continue
+
+        is_task_completed = chunk.get("is_task_complete")
+
+        # TODO: handle the case when input is required
+        if is_task_completed:
+            await publish_generator_completed(task_id, session_id, content)
+        else:
+            tag_html = accumulator.append_and_return_html(content)
+            if not tag_html:
+                # Accumulate more text before publishing chunk.
+                continue
+            logger.info(f"HTML tag exists: {tag_html}")
+            await publish_artifact_update(task_id, session_id, tag_html)
+
+
 async def process_user_message(session_id, message: str):
     url = os.getenv("RECEIVE_URL", "http://0.0.0.0:8080")
     agent_message = Message(role="user", parts=[TextPart(text=message)])
@@ -72,9 +124,7 @@ async def process_user_message(session_id, message: str):
     if is_task_completed:
         await publish_task_request(session_id, session_id, content)
         await publish_task_response(session_id, session_id, content, TaskState.COMPLETED)
+        await start_streaming_task(session_id, session_id, content)
     else:
         await publish_task_response(session_id, session_id, content,
-                              TaskState.INPUT_REQUIRED)
-
-
-
+                                    TaskState.INPUT_REQUIRED)
--- generator_agent/start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-
-# subscribe waits for server to start up
-python subscribe_to_pubsub.py &
-python ./app/main.py
