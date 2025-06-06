fec452efc8aeba57e2a1b487ec1522abf7b747b3
Yason Khaburzaniya <yason@hey.com>
2025-06-05 17:21:21 -0700

    deploy sites to breba-sites folder

--- breba_app/app/site_upload.py
+++ breba_app/app/site_upload.py
@@ -2,6 +2,14 @@ from google.cloud import storage
 import os
 
 def upload_site(local_path: str, site_name: str, bucket_name: str = "breba-sites"):
+    """
+    Uploads site to google cloud
+    Example: upload_site("/Users/yason/breba/disc-site/sites/test-site", "test-site")
+    :param local_path: relative or absolute path of local directory
+    :param site_name: site name where all the files will be stored
+    :param bucket_name: name of the bucket
+    :return:
+    """
     client = storage.Client()
     bucket = client.bucket(bucket_name)
 
@@ -11,7 +19,7 @@ def upload_site(local_path: str, site_name: str, bucket_name: str = "breba-sites
         for file in files:
             file_path = os.path.join(root, file)
             rel_path = os.path.relpath(file_path, local_path)
-            blob_path = f"sites/{site_name}/{rel_path}"
+            blob_path = f"{site_name}/{rel_path}"
 
             blob = bucket.blob(blob_path)
             blob.upload_from_filename(file_path)
@@ -19,9 +27,7 @@ def upload_site(local_path: str, site_name: str, bucket_name: str = "breba-sites
 
             print(f"Uploaded: {blob_path}")
 
-    blob = bucket.blob(f"sites/{site_name}/index.html")
+    blob = bucket.blob(f"{site_name}/index.html")
     url = f"https://storage.googleapis.com/{bucket.name}/{blob.name}"
     return url
 
-# Example usage
-upload_site("/Users/yason/breba/disc-site/sites/test-site", "test-site")
