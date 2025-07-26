b83c9d5c53d330c084ea9eb9ea35b330d6a8eb6c
Yason Khaburzaniya <yason@hey.com>
2025-07-26 14:53:12 -0700

    allow sending messages to generator directly

--- breba_app/app/orchestrator.py
+++ breba_app/app/orchestrator.py
@@ -1,3 +1,4 @@
+import difflib
 import logging
 
 from builder_agent.agent import agent as builder_agent
@@ -14,6 +15,17 @@ def get_generator_response(session_id: str):
 
 def set_generator_response(session_id: str, html_output: str):
     generator_agent.set_last_html(session_id, html_output)
+
+
+def get_html_diff(old_html: str, new_html: str):
+    diff = difflib.unified_diff(
+        old_html.splitlines(),
+        new_html.splitlines(),
+        fromfile='a',
+        tofile='b',
+        lineterm=''
+    )
+    return "\n".join(diff)
 
 
 async def start_streaming_task(user_name: str, session_id: str, query: str, generator_callback):
@@ -36,6 +48,33 @@ async def start_streaming_task(user_name: str, session_id: str, query: str, gene
                 continue
             logger.info(f"HTML tag exists: {tag_html}")
             await generator_callback(tag_html)
+
+
+async def to_generator(user_name: str, session_id: str, message: str, builder_completed_callback, generator_callback,
+                       message_to_user_callback):
+    old_html = generator_agent.get_last_html(session_id)
+    await message_to_user_callback("Generator is processing your request...")
+    await start_streaming_task(user_name, session_id, message, generator_callback)
+    new_html = generator_agent.get_last_html(session_id)
+    diff = get_html_diff(old_html, new_html)
+
+    agent_message = Message(role="user", parts=[TextPart(text=f"After user left this: {message} \n"
+                                                              f"\n The generator modified the output according to this diff {diff}.\n"
+                                                              f"Modify website specification to accommodate user request, but only if the root cause of the problem stemmed from the insufficient website specification. \n"
+                                                              f"IMPORTANT: If the diff shows that an the issued stemmed from a bug in the implementation, do not modify the website specification.")])
+
+    # TODO: should probably ask user to confirm
+    await message_to_user_callback("Rebuilding the specification...")
+    agent_response = await builder_agent.invoke(user_name, session_id, agent_message)
+
+    content = agent_response.get("content")
+    is_task_completed = agent_response.get("is_task_complete")
+
+    if is_task_completed:
+        await builder_completed_callback(content)
+    else:
+        logger.info(f"Waiting for user input: {content}")
+        await message_to_user_callback(content)
 
 
 async def to_builder(user_name: str, session_id: str, message: str, builder_completed_callback,
