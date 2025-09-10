4e48c8d084412c8928b7ccb1a1c4853ef1905b15
Yason Khaburzaniya <yason@hey.com>
2025-09-10 08:58:59 -0700

    cleanup code

--- breba_app/storage.py
+++ breba_app/storage.py
@@ -9,9 +9,6 @@ from typing import Tuple, TypedDict, Union
 import boto3
 from botocore.exceptions import BotoCoreError, ClientError
 from dotenv import load_dotenv
-from google.api_core.exceptions import NotFound
-from google.cloud import storage
-from google.cloud.storage import Bucket, Blob
 
 load_dotenv()
 
@@ -20,7 +17,6 @@ logger = logging.getLogger(__name__)
 USERS_BUCKET_NAME: str = os.getenv("USERS_BUCKET")
 CLOUDFLARE_ENDPOINT: str = os.getenv("CLOUDFLARE_ENDPOINT")
 CDN_BASE_URL: str = os.getenv("CDN_BASE_URL") or "https://cdn.breba.app"
-
 
 session = boto3.session.Session()
 # Uses AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY from the environment
@@ -46,31 +42,9 @@ def public_file_url(user_name: str, session_id: str, file_name: str) -> str:
     return f"{CDN_BASE_URL}/{user_name}/{session_id}/{file_name}"
 
 
-# def _copy_directory(
-#         source_bucket_name: str,
-#         target_bucket_name: str,
-#         prefix: str,  # e.g., "some/folder/"
-#         target_prefix: str = None  # Optional new prefix in target
-# ):
-#     source_bucket = storage_client.bucket(source_bucket_name)
-#     target_bucket = storage_client.bucket(target_bucket_name)
-#
-#     blobs = storage_client.list_blobs(source_bucket, prefix=prefix)
-#
-#     # TODO: optimize this using asyncio.to_thread or create_task
-#     for blob in blobs:
-#         # Compute new destination name
-#         relative_path = blob.name[len(prefix):]
-#         new_prefix = target_prefix if target_prefix is not None else prefix
-#         new_name = f"{new_prefix}{relative_path}"
-#
-#         # Copy blob to target
-#         new_blob = source_bucket.copy_blob(blob, target_bucket, new_name)
-#
-#         logger.info(f"Copied {blob.name} -> {new_name}")
-
 def _copy_directory_s3(source_bucket, target_bucket, prefix: str, target_prefix: str = None):
     try:
+        # TODO: optimize this using asyncio.to_thread or create_task
         for obj_summary in source_bucket.objects.filter(Prefix=prefix):
             relative_path = obj_summary.key[len(prefix):]
             new_prefix = target_prefix if target_prefix else prefix
@@ -83,12 +57,6 @@ def _copy_directory_s3(source_bucket, target_bucket, prefix: str, target_prefix:
         logger.error(f"Error copying directory from {source_bucket.name} to {target_bucket.name}: {e}")
         raise
 
-
-# def _user_session_blob(user_name: str, session_id: str, relative_path: str, description: str | None = None) -> Blob:
-#     blob = private_bucket.blob(f"{user_name}/{session_id}/{relative_path}")
-#     if description is not None:
-#         blob.metadata = {"description": description}
-#     return blob
 
 def _user_session_object(user_name: str, session_id: str, relative_path: str, description: str | None = None):
     key = f"{user_name}/{session_id}/{relative_path}"
@@ -135,10 +103,6 @@ def save_image_file_to_private(user_name: str, session_id: str, file_name: str, 
         raise
 
 
-# def save_spec(user_name: str, session_id: str, spec: str):
-#     blob = _user_session_blob(user_name, session_id, "spec.txt")
-#     blob.upload_from_string(spec)
-
 def save_spec(user_name: str, session_id: str, spec: str):
     key = f"{user_name}/{session_id}/spec.txt"
     s3_client.put_object(
@@ -149,13 +113,6 @@ def save_spec(user_name: str, session_id: str, spec: str):
     )
 
 
-# def read_spec_text(user_name: str, session_id: str) -> str | None:
-#     try:
-#         blob = _user_session_blob(user_name, session_id, "spec.txt")
-#         return blob.download_as_string().decode("utf-8")
-#     except NotFound:
-#         return None
-
 def read_spec_text(user_name: str, session_id: str) -> str | None:
     key = f"{user_name}/{session_id}/spec.txt"
     try:
@@ -165,24 +122,10 @@ def read_spec_text(user_name: str, session_id: str) -> str | None:
         return None
 
 
-# def read_index_html(user_name: str, session_id: str) -> str:
-#     blob = _user_session_blob(user_name, session_id, "index.html")
-#     return blob.download_as_string().decode("utf-8")
-
 def read_index_html(user_name: str, session_id: str) -> str:
     key = f"{user_name}/{session_id}/index.html"
     obj = s3.Object(USERS_BUCKET_NAME, key)
     return obj.get()["Body"].read().decode("utf-8")
-
-
-# def read_image_from_private(user_name: str, session_id: str, image_name: str) -> Tuple[bytes, dict[str, str]] | None:
-#     blob = private_bucket.blob(f"{user_name}/{session_id}/images/{image_name}")
-#
-#     if not blob.exists():
-#         return None
-#
-#     blob.reload()
-#     return blob.download_as_bytes(), blob.metadata
 
 
 def read_image_from_private(user_name: str, session_id: str, image_name: str) -> Tuple[bytes, dict[str, str]] | None:
@@ -207,13 +150,6 @@ def save_file_to_private(user_name: str, session_id: str, file_name: str, conten
         ContentType=content_type
     )
 
-
-# def load_template(user_name: str, session_id: str, template_name: str):
-#     _copy_directory(
-#         source_bucket_name=private_bucket.name, target_bucket_name=private_bucket.name,
-#         prefix=f"templates/{template_name}",
-#         target_prefix=f"{user_name}/{session_id}"
-#     )
 
 def load_template(user_name: str, session_id: str, template_name: str):
     _copy_directory_s3(
@@ -256,26 +192,6 @@ def list_s3_structured(user_name: str, session_id: str) -> DirTree:
     return files
 
 
-# def list_files_structured(user_name: str, session_id: str) -> DirTree:
-#     prefix = f"{user_name}/{session_id}"
-#     blobs = private_bucket.list_blobs(prefix=prefix)
-#     files = make_dir_tree()
-#
-#     for blob in blobs:
-#         parts = blob.name[len(prefix) + 1:].split("/")
-#         file_name = parts[-1]
-#
-#         file = register_file(parts, files)
-#         if blob.metadata:
-#             file[file_name] = {
-#                 "__description__": blob.metadata.get("description", "No description")
-#             }
-#         else:
-#             file[file_name] = {"__description__": "No description"}
-#
-#     return files
-
-
 def format_tree(tree: DirTree, indent=0):
     lines = []
     for key, value in sorted(tree.items()):
@@ -299,30 +215,20 @@ def get_public_url(site_name: str) -> str:
     return f"https://{site_name}.breba.site"
 
 
-# # TODO: user_name/session_id are state for the entire request, should probably create a user_cloud_storage class
-# def upload_site(user_name: str, session_id: str, site_name: str):
-#     """
-#     Uploads site to google cloud
-#     Example: upload_site("/Users/yason/breba/disc-site/sites/test-site", "test-site")
-#     :param user_name: username
-#     :param session_id: session id used for locating site files
-#     :param site_name: site name where all the files will be stored
-#     :return: public url of deployed site
-#     """
-#     # TODO: convert to async because GCP is a blocking call, we don't want to have to wait
-#     # Sanitize site name
-#     site_name = re.sub("[^0-9a-zA-Z]+", "-", site_name).strip(" -")
-#     # TODO: when empty dir is being uploaded, should pass back an error message
-#     _copy_directory(
-#         source_bucket_name=private_bucket.name,
-#         target_bucket_name=public_bucket.name, prefix=f"{user_name}/{session_id}", target_prefix=site_name
-#     )
-#
-#     return get_public_url(site_name)
-
+# TODO: user_name/session_id are state for the entire request, should probably create a user_cloud_storage class
 def upload_site(user_name: str, session_id: str, site_name: str):
+    """
+    #     Uploads site to google cloud
+    #     Example: upload_site("/Users/yason/breba/disc-site/sites/test-site", "test-site")
+    #     :param user_name: username
+    #     :param session_id: session id used for locating site files
+    #     :param site_name: site name where all the files will be stored
+    #     :return: public url of deployed site
+    #     """
     site_name = re.sub("[^0-9a-zA-Z]+", "-", site_name).strip(" -")
 
+    # TODO: convert to async because GCP is a blocking call, we don't want to have to wait
+    # TODO: when empty dir is being uploaded, should pass back an error message
     _copy_directory_s3(
         source_bucket=s3_bucket,
         target_bucket=public_s3_bucket,
