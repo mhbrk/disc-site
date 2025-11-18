d1d198f397eddb3339d537a34ffb83bd03425aa3
Yason Khaburzaniya <yason@hey.com>
2025-11-17 17:52:26 -0800

    fix product name prompt to make sure it just returns the product name

--- breba_app/llm_utils.py
+++ breba_app/llm_utils.py
@@ -6,7 +6,8 @@ client = AsyncOpenAI()
 async def get_product_name(description: str) -> str:
     prompt = (
         f"Create a product name given the description. *Important:* your response must be one to three words long."
-        f" Description: {description}")
+        f" <BEGIN DESCRIPTION>\n{description}\n<END DESCRIPTION>\n\n"
+        f"**Your response must be a one to three words long description of the product above.**")
 
     response = await client.responses.create(model="gpt-4.1-nano", input=prompt)
 
