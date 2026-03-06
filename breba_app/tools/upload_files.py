7e3ef7741550694e5da22b70e8829e60d4e2d5a2
Yason Khaburzaniya <yason@hey.com>
2026-03-05 20:19:23 -0800

    allow uploading non-picture files

--- breba_app/tools/upload_files.py
+++ breba_app/tools/upload_files.py
@@ -1,6 +1,8 @@
 import asyncio
+import logging
 from pathlib import Path
 
+import PIL
 from PIL import Image
 
 from breba_app.storage import save_image_file_to_private
@@ -8,15 +10,20 @@ from breba_app.storage import save_image_file_to_private
 MAX_CONCURRENT_UPLOADS = 10
 sem = asyncio.Semaphore(MAX_CONCURRENT_UPLOADS)
 
+logger = logging.getLogger()
+
 def get_image_dimensions(path: Path):
     with Image.open(path) as img:
         return img.width, img.height
 
 async def process_file(user_name: str, product_id: str, file_path: Path, file_name: str, description: str):
     async with sem:
-        # get dimensions in a thread (Pillow is blocking)
-        width, height = await asyncio.to_thread(get_image_dimensions, file_path)
-        description = f"{description}. Image dimensions: {width}x{height}"
+        try:
+            # get dimensions in a thread (Pillow is blocking)
+            width, height = await asyncio.to_thread(get_image_dimensions, file_path)
+            description = f"{description}. Image dimensions: {width}x{height}"
+        except PIL.UnidentifiedImageError:
+            logger.info("Uploaded file is not an image. Will not attach image metadata")
 
         blob_path = await asyncio.to_thread(
             save_image_file_to_private,
